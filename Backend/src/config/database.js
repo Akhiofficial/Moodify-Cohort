9bfb434c127f7de1082e2ed9f808a02ad4333029
Akhilesh Mandawgane <akhileshmandawganeofficial@gmail.com>
2026-03-05 00:03:05 +0530

    feat:Setup for deployemnt in folder structure

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 
 function connectToDb() {
     mongoose.connect(process.env.MONGO_URI)
-    .then(()=> {
-        console.log("Connected To DB");
-    })
+        .then(() => {
+            console.log("Connected To DB");
+        })
+        .catch((err) => {
+            console.error("DB Connection Error:", err.message);
+        })
 }
 
 module.exports = connectToDb;
